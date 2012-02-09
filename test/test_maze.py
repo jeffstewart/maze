366a03afc00ca7d958a69a9dbb59471e7e130365
Jeff Stewart <jeffstewart.ca@gmail.com>
2012-02-09 13:06:43 -0800

    Added a couple more tests

--- test/test_maze.py
+++ test/test_maze.py
@@ -29,4 +29,56 @@ X....
     _stdout = StringIO()
     maze.main(_stdin=StringIO(input), _stdout=_stdout)
     assert_equal(_stdout.getvalue(), expected)
-    
+
+
+def test_unsolvable1():
+
+    # Tests a completely filled maze.
+    input = """\
+XXXXXXXXX
+XXXXXXXXX
+XXXXXXXXX
+XXXXXXXXX
+XXXXXXXXX
+XXXXXXXXX
+"""
+
+    expected = "Not solvable\n"
+
+    _stdout = StringIO()
+    maze.main(_stdin=StringIO(input), _stdout=_stdout)
+    assert_equal(_stdout.getvalue(), expected)
+
+
+def test_unsolvable2():
+
+    # Test the origin is not available.
+    input = """\
+X
+
+
+
+"""
+
+    expected = "Not solvable\n"
+
+    _stdout = StringIO()
+    maze.main(_stdin=StringIO(input), _stdout=_stdout)
+    assert_equal(_stdout.getvalue(), expected)
+
+
+def test_unsolvable3():
+
+    # Test the end point not available.
+    input = """\
+
+
+
+   X
+"""
+
+    expected = "Not solvable\n"
+
+    _stdout = StringIO()
+    maze.main(_stdin=StringIO(input), _stdout=_stdout)
+    assert_equal(_stdout.getvalue(), expected)
